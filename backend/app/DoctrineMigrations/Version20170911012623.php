436db15e2e91be6ed6da6d1a0868992af2f3693c
Islam Askar <islam.askar@gmail.com>
2017-09-11 03:33:22 +0200

    Create a migration for the Apartment table

---
+++ backend/app/DoctrineMigrations/Version20170911012623.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170911012623 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE apartment (id INT AUTO_INCREMENT NOT NULL, move_in_date DATE NOT NULL, street VARCHAR(255) NOT NULL, post_code INT NOT NULL, town VARCHAR(100) NOT NULL, country VARCHAR(50) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE apartment');
+    }
+}
--- backend/src/AppBundle/Entity/Apartment.php
+++ backend/src/AppBundle/Entity/Apartment.php
@@ -218,4 +218,3 @@ class Apartment
         return $this->email;
     }
 }
-
